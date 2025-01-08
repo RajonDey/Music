834ca97045ca3ecea926892436f584b932aea5ae
RajonDey <rajondeyofficial@gmail.com>
2025-01-08 22:06:05 +0600

    home page added

--- themes/RDMusic/footer.php
+++ themes/RDMusic/footer.php
@@ -1,8 +1,5 @@
 <footer id="site-footer">
     <div class="container">
-        <div class="footer-widgets">
-            <?php dynamic_sidebar('footer-widget-area'); ?>
-        </div>
         <div class="site-info">
             &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
         </div>
--- themes/RDMusic/templates/front-page.php
+++ themes/RDMusic/templates/front-page.php
@@ -1,10 +1,28 @@
-<?php get_header(); ?>
+<?php
+/*
+Template Name: Home Screen
+*/
 
-<div class="container">
-    <main id="main-content">
-        Home Page
-    </main>
+// Get the header
+get_header();
+?>
+
+
+
+<div id="page" class="site">
+    <div id="content" class="site-content">
+        <video autoplay muted loop id="myVideo">
+            <source src="<?php echo get_template_directory_uri(); ?>/assets/video/rdmusic-banner.mp4" type="video/mp4">
+        </video>
+        <div class="overlay">
+           NEWSLETTER + Blog<br/>
+           Insta/Twitter/Facebook/
+           Soundcloud/
+        </div>
+    </div>
 </div>
 
-<?php get_footer(); ?>
-
+<?php
+// Get the footer
+get_footer();
+?>
