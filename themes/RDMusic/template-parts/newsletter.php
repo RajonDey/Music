cc7100de5371e2e08f5a94ea82e0c7e21571d11e
RajonDey <rajondeyofficial@gmail.com>
2025-01-10 16:44:48 +0600

    fixing again

--- themes/RDMusic/template-parts/newsletter.php
+++ themes/RDMusic/template-parts/newsletter.php
@@ -1,5 +1,6 @@
 <!-- Newsletter Form -->
-<form class="newsletter-form" action="https://rd-beats.beehiiv.com/subscribe" method="post">
+<!-- <form class="newsletter-form" action="https://rd-beats.beehiiv.com/subscribe" method="post">
     <input type="email" name="EMAIL" placeholder="Your email address" required>
     <button type="submit">Subscribe</button>
-</form>
+</form> -->
+<iframe class="beehiiv-newsletter-form" src="https://embeds.beehiiv.com/c893af3a-0ad7-455c-b91a-ea51b01cb61c?slim=true" data-test-id="beehiiv-embed" height="52" frameborder="0" scrolling="no" style="margin: 0; border-radius: 0px !important; background-color: transparent;"></iframe>
